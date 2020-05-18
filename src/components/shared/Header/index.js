9fe1c20b020cfe93ba9c84a0f56c9f4797c5f06e
reubert22 <reubertfernandes@gmail.com>
2020-05-17 21:46:34 -0300

    Loading on home and series view

--- src/components/shared/Header/index.js
+++ src/components/shared/Header/index.js
@@ -5,33 +5,38 @@ import { BaseButton } from 'react-native-gesture-handler';
 import { View, StyleSheet } from 'react-native';
 import FastImage from 'react-native-fast-image';
 
-import { posterOriginal } from '../../../utils/constants';
+import { posterOriginal, primaryColor } from '../../../utils/constants';
+import Loading from '../Loading';
 
-const Header = ({ latest, onDetails }) => (
-  <BaseButton onPress={() => onDetails(latest, 0)} style={styles.container}>
-    <SharedElement style={styles.container} id={`image-${latest.id}-0`}>
-      <FastImage
-        style={styles.image}
-        source={{
-          uri: `${posterOriginal}${latest.poster_path}`,
-          priority: FastImage.priority.normal,
+const Header = ({ latest, onDetails, isLoading }) => isLoading ? (
+  <View style={{ ...styles.container, alignItems: 'center', justifyContent: 'center' }}>
+    <Loading color={primaryColor} />
+  </View>
+) : (
+    <BaseButton onPress={() => onDetails(latest, 0)} style={styles.container}>
+      <SharedElement style={styles.container} id={`image-${latest.id}-0`}>
+        <FastImage
+          style={styles.image}
+          source={{
+            uri: `${posterOriginal}${latest.poster_path}`,
+            priority: FastImage.priority.normal,
+          }}
+          resizeMode={FastImage.resizeMode.stretch}
+        />
+      </SharedElement>
+      <View
+        style={{
+          position: 'absolute',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          bottom: 10,
         }}
-        resizeMode={FastImage.resizeMode.stretch}
-      />
-    </SharedElement>
-    <View
-      style={{
-        position: 'absolute',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bottom: 10,
-      }}
-    >
-      <ChevronIco name="chevron-up" size={20} color="#fff" />
-    </View>
-  </BaseButton>
-)
+      >
+        <ChevronIco name="chevron-up" size={20} color="#fff" />
+      </View>
+    </BaseButton>
+  )
 
 const styles = StyleSheet.create({
   container: {
